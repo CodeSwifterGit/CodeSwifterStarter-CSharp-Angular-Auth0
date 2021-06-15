e07c5b1375df5a948529552241e889035be737a2
Hrvoje Matić <hmatic5@outlook.com>
2021-06-15 23:09:44 +0200

    Fix usings.

--- src/CodeSwifterStarter.Common/Models/SingleSignOn.cs
+++ src/CodeSwifterStarter.Common/Models/SingleSignOn.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodeSwifterStarter.Common.Models
+﻿namespace CodeSwifterStarter.Common.Models
 {
     public class SingleSignOn
     {
